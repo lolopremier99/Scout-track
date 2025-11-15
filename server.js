1961ed5322c9141f8129d3cc1cebbf382abbcef0
lolopremier99 <lolopremier@gmail.com>
2025-11-15 08:33:08 +0000

    serveur corrigé

--- server.js
+++ server.js
@@ -10,13 +10,15 @@ app.get("/hello", (req, res) => {
   res.send("Bonjour Grand Maître 👑 depuis ton serveur !");
 });
 
-const PORT = 3000;
+// Port du serveur (prend la variable d'environnement PORT si elle existe)
+const PORT = process.env.PORT || 3000;
 
 // Obtenir l'adresse IP locale pour le réseau
 const ip = Object.values(os.networkInterfaces())
   .flat()
   .filter(i => i?.family === "IPv4" && !i.internal)[0]?.address;
 
+// Lancer le serveur
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Serveur lancé sur le port ${PORT}`);
   console.log(`Local: http://localhost:${PORT}`);
